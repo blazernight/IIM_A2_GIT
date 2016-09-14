40688e7ec73679bf4249fefce72cd86fd5a23410
sally cohen <saliloucohen18@yahoo.fr>
2016-09-14 17:12:16 +0200

    validation update

--- validation.php
+++ validation.php
@@ -1,14 +1,18 @@
-<?php
+<?php session_start();
 require('config/config.php');
 require('model/functions.fn.php');
-session_start();
 
-if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
-	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
-	// TODO
+if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) 
+&& !empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) 
+{
+
+	userRegistration($db, $_POST['username'], $_POST['email'], $_POST['password']);
+	header('Location: login.php');
 
 }else{ 
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
 	header('Location: register.php');
 }
+
+

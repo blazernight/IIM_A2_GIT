bca323a8b6aa7762d5f97c349586db41731f7b57
mathieu duquesne <math.duquesne@laposte.net>
2016-09-14 17:22:47 +0200

    validation

--- IIM_Git_SoundCloud.sql
+++ IIM_Git_SoundCloud.sql
@@ -1,99 +1,80 @@
--- phpMyAdmin SQL Dump
--- version 4.5.0.2
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Ven 02 Octobre 2015 à 13:55
--- Version du serveur :  10.0.21-MariaDB-log
--- Version de PHP :  5.6.13
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Hôte: localhost (MySQL 5.7.11)
+# Base de données: IIM_GIT
+# Temps de génération: 2016-09-14 12:29:22 +0000
+# ************************************************************
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Base de données :  `IIM_Git_SoundCloud`
---
 
--- --------------------------------------------------------
+# Affichage de la table musics
+# ------------------------------------------------------------
 
---
--- Structure de la table `musics`
---
+DROP TABLE IF EXISTS `musics`;
 
 CREATE TABLE `musics` (
-  `id` int(11) UNSIGNED NOT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL COMMENT 'refers to id in users table',
   `title` varchar(100) NOT NULL,
   `file` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Contenu de la table `musics`
---
+LOCK TABLES `musics` WRITE;
+/*!40000 ALTER TABLE `musics` DISABLE KEYS */;
 
-INSERT INTO `musics` (`id`, `user_id`, `title`, `file`, `created_at`) VALUES
-(1, 1, 'UN*DEUX - Shopping Day', 'musics/d0dbde0148d66ddf8ae815e014e2a668.1.mp3', '2015-10-01 13:35:05'),
-(2, 1, 'FlicFlac - Can''t Get Away (Bootleg)', 'musics/4baf839a4706fdc8caf286cd35dba410.1.mp3', '2015-10-02 11:41:26');
+INSERT INTO `musics` (`id`, `user_id`, `title`, `file`, `created_at`)
+VALUES
+	(1,1,'UN*DEUX - Shopping Dayk','musics/d0dbde0148d66ddf8ae815e014e2a668.1.mp3','2015-10-01 13:35:05');
 
--- --------------------------------------------------------
+/*!40000 ALTER TABLE `musics` ENABLE KEYS */;
+UNLOCK TABLES;
 
---
--- Structure de la table `users`
---
+
+# Affichage de la table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
-  `id` int(11) UNSIGNED NOT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL COMMENT 'encrypted passwords are better',
-  `picture` varchar(255) NOT NULL COMMENT 'name of profile picture',
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `picture` varchar(255) DEFAULT '' COMMENT 'name of profile picture',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Contenu de la table `users`
---
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `picture`, `created_at`) VALUES
-(1, 'Git', 'git@initiation.com', 'password', 'view/profil_pic/e8df43b8a90546b15da8591c89711879.1.jpg', '2015-10-01 11:13:46');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `picture`, `created_at`)
+VALUES
+	(1,'Git','git@initiation.com','password','view/profil_pic/f26c44906efcbfb8e80baaf4dafd0134.1.png','2015-10-01 11:13:46');
 
---
--- Index pour les tables exportées
---
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
---
--- Index pour la table `musics`
---
-ALTER TABLE `musics`
-  ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `musics`
---
-ALTER TABLE `musics`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- validation.php
+++ validation.php
@@ -6,7 +6,21 @@ session_start();
 if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
-	// TODO
+	$isEmailAvailable = isEmailAvailable($db, $_POST['email']);
+	$isUsernameAvailable = isUsernameAvailable($db, $_POST['username']);
+	if($isEmailAvailable == true && $isUsernameAvailable == true){
+		$userRegistration = userRegistration($db, $_POST['username'], $_POST['email'], $_POST['password']);
+		header('Location: login.php');
+	}
+	elseif($isEmailAvailable == false && $isUsernameAvailable == true){
+		$_SESSION['message'] = 'Erreur : Email non disponible';
+		header('Location: register.php');
+	}
+	elseif($isEmailAvailable == true && $isUsernameAvailable == false){
+		$_SESSION['message'] = 'Erreur : Username non disponible';
+		header('Location: register.php');
+	}
+
 
 }else{ 
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
